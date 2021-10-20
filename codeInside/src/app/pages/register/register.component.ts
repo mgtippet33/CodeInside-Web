42bc4a610a27b683954604fee522561ffb0006e2
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-10-20 21:02:41 +0300

    add registration page

--- codeInside/src/app/pages/register/register.component.ts
+++ codeInside/src/app/pages/register/register.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component  } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
+
 export class RegisterComponent {
-  title = 'CodeInside';
+  public showPassword: boolean;
+  public showConfirmPassword: boolean;
+  constructor(private router: Router) {
+    this.showPassword = false;
+    this.showConfirmPassword = false;
+  }
+
+
+  onRegisterClick(): void{
+    
+  }
+  
+  onBackToLoginClick(): void{
+    this.router.navigateByUrl('/login');
+  }
 }
