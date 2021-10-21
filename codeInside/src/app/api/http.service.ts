d880fe38f6f4dc8dd46d40c22142f4616b641dc9
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-10-21 23:21:54 +0300

    fixed some bugs

--- codeInside/src/app/api/http.service.ts
+++ codeInside/src/app/api/http.service.ts
@@ -1,18 +1,22 @@
-import {Injectable} from '@angular/core';
-import {HttpClient, HttpResponse} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { User } from '../Models/user';
 import { ApiConstants } from './ApiConstants';
-import { map } from "rxjs/operators"; 
+
 
 @Injectable()
-export class HttpService{
-   
-    constructor(private http: HttpClient){ }
- 
-    postData(user: User){
-        const headers = { 'content-type': 'application/json'}  
-        const body = JSON.stringify({email: user.email, name: user.username,
-            birthday: user.birthday, password: user.password });
-        return this.http.post(ApiConstants.register_url, body,{'headers':headers, observe: 'response'});
+export class HttpService {
+
+    constructor(private http: HttpClient) { }
+
+    registerUser(user: User) {
+        const headers = { 'content-type': 'application/json' }
+        const body = {
+            email: user.email,
+            name: user.username,
+            birthday: user.birthday,
+            password: user.password
+        };
+        return this.http.post(ApiConstants.register_url, body, { 'headers': headers, observe: 'response' });
     }
 }
--- codeInside/src/app/pages/register/register.component.ts
+++ codeInside/src/app/pages/register/register.component.ts
@@ -6,71 +6,78 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { formatDate } from '@angular/common';
 
 @Component({
-  selector: 'register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss'],
-  providers: [HttpService]
+    selector: 'register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss'],
+    providers: [HttpService]
 })
 
 export class RegisterComponent {
-  public user!: User;
-  public registrationForm!: FormGroup;
-  public showPassword: boolean;
-  public showConfirmPassword: boolean;
-  public userClick: boolean = false;
+    public user: User;
+    public registrationForm!: FormGroup;
+    public showPassword: boolean;
+    public showConfirmPassword: boolean;
+    public userClick: boolean = false;
 
-  constructor(private httpService: HttpService, private router: Router) {
-    this.showPassword = false;
-    this.showConfirmPassword = false;
-  }
-
-  ngOnInit(): void {
-    this.registrationForm = new FormGroup({
-      email: new FormControl('',
-        [
-          Validators.required,
-          Validators.email
-        ]),
-      username: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3)
-      ]),
-      birthday: new FormControl('', 
-      // [
-      //   Validators.required,
-      //   Validators.pattern('d{1,2}/\d{1,2}/\d{4}')
-      // ]
-      ),
-      password: new FormControl('', 
-      // [
-      //   Validators.required,
-      //   Validators.pattern('^(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$')
-      // ]
-      ),
-      confirmPassword: new FormControl(''),
-    });
-  }
-
-  onRegisterClick(): void {
-    this.userClick = true;
-    if(this.registrationForm.valid) {
-      this.user = new User(this.registrationForm.value['email'], this.registrationForm.value['username'],
-      formatDate(this.registrationForm.value['birthday'], 'dd/MM/yyyy', 'en-US'), this.registrationForm.value['password'],
-      false, true, false, false, []);
-    this.httpService.postData(this.user)
-      .subscribe(response => {
-        if (response.status == 201) {
-          console.log("User successfuly register")
-          this.router.navigateByUrl('/login');
-        }
-        else{
-          // TODO - modal view about current email exists in the system   
-        }
-      });
+    constructor(private httpService: HttpService, private router: Router) {
+        this.showPassword = false;
+        this.showConfirmPassword = false;
     }
-  }
 
-  onBackToLoginClick(): void {
-    this.router.navigateByUrl('/login');
-  }
+    ngOnInit(): void {
+        this.registrationForm = new FormGroup({
+            email: new FormControl('',
+                [
+                    Validators.required,
+                    Validators.email
+                ]),
+            username: new FormControl('', [
+                Validators.required,
+                Validators.minLength(3)
+            ]),
+            birthday: new FormControl('',
+                // [
+                //   Validators.required,
+                //   Validators.pattern('d{1,2}/\d{1,2}/\d{4}')
+                // ]
+            ),
+            password: new FormControl('',
+                // [
+                //   Validators.required,
+                //   Validators.pattern('^(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$')
+                // ]
+            ),
+            confirmPassword: new FormControl(''),
+        });
+    }
+
+    async onRegisterClick(): Promise<void> {
+        this.userClick = true;
+        if (this.registrationForm.valid) {
+            this.user = {
+                email: this.registrationForm.value['email'],
+                username: this.registrationForm.value['username'],
+                birthday: formatDate(this.registrationForm.value['birthday'], 'dd/MM/yyyy', 'en-US'),
+                password: this.registrationForm.value['password'],
+                premium: false,
+                active: true,
+                admin: false,
+                moderator: false,
+                achievement: []
+            };
+            const response = await this.httpService.registerUser(this.user)
+                .toPromise();
+                if (response.status == 201) {
+                    console.log("User successfuly register")
+                    this.router.navigateByUrl('/login');
+                }
+                else {
+                    // TODO - modal view about current email exists in the system   
+                }
+        }
+    }
+
+    onBackToLoginClick(): void {
+        this.router.navigateByUrl('/login');
+    }
 }
