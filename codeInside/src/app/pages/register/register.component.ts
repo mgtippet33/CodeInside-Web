d35b28bf4ce63f843dd8ba933a62fc49f68b9258
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-10-21 21:39:12 +0300

    fixed registration api added some validators

--- codeInside/src/app/pages/register/register.component.ts
+++ codeInside/src/app/pages/register/register.component.ts
@@ -1,9 +1,10 @@
-import { Component  } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/Models/user';
 import { HttpService } from 'src/app/api/http.service';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { formatDate } from '@angular/common';
+
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
@@ -16,41 +17,57 @@ export class RegisterComponent {
   public registrationForm!: FormGroup;
   public showPassword: boolean;
   public showConfirmPassword: boolean;
+  public userClick: boolean = false;
 
-  done: boolean = false;
-  statusCode: string | undefined;
-  constructor(private httpService:HttpService, private router: Router) {
+  constructor(private httpService: HttpService, private router: Router) {
     this.showPassword = false;
     this.showConfirmPassword = false;
   }
 
   ngOnInit(): void {
-    this.initForm();
-  }
-
-  initForm(): void {
     this.registrationForm = new FormGroup({
-        email: new FormControl(''),
-        username: new FormControl(''),
-        birthday: new FormControl(''),
-        password: new FormControl(''),
+      email: new FormControl('',
+        [
+          Validators.required,
+          Validators.email
+        ]),
+      username: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      birthday: new FormControl('', 
+      // [
+      //   Validators.required,
+      //   Validators.pattern('d{1,2}/\d{1,2}/\d{4}')
+      // ]
+      ),
+      password: new FormControl('', 
+      // [
+      //   Validators.required,
+      //   Validators.pattern('^(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$')
+      // ]
+      ),
+      confirmPassword: new FormControl(''),
     });
   }
 
-  onRegisterClick(): void{
-    console.log(this.registrationForm.value);
-    this.user = new User(this.registrationForm.value['email'], this.registrationForm.value['username'],
-    formatDate(this.registrationForm.value['birthday'], 'dd/MM/yyyy', 'en-US'), this.registrationForm.value['password'],
-    false, true, false, false, []);
-    console.log(this.user);
+  onRegisterClick(): void {
+    this.userClick = true;
+    if(this.registrationForm.valid) {
+      this.user = new User(this.registrationForm.value['email'], this.registrationForm.value['username'],
+      formatDate(this.registrationForm.value['birthday'], 'dd/MM/yyyy', 'en-US'), this.registrationForm.value['password'],
+      false, true, false, false, []);
     this.httpService.postData(this.user)
-                .subscribe(
-                    (data: any) => {this.statusCode=data['status code']; this.done=true;},
-                    error => console.log(error)
-                );
+      .subscribe(response => {
+        if (response.status == 201) {
+          console.log("User successfuly register")
+          this.router.navigateByUrl('/login');
+        }
+      });
+    }
   }
-  
-  onBackToLoginClick(): void{
+
+  onBackToLoginClick(): void {
     this.router.navigateByUrl('/login');
   }
 }
