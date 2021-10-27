b0825f5997300ea50ce53b626e5a0c1c405237b4
anastasiiachervotochenko <anastasiia.chervotochenko@nure.ua>
2021-10-27 07:48:43 +0300

    Implement structure of task list page

--- codeInside/src/app/pages/mainPage/mainPage.component.ts
+++ codeInside/src/app/pages/mainPage/mainPage.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { Task } from 'src/app/Models/task';
 
 @Component({
     selector: 'main',
@@ -7,7 +8,13 @@ import { Router } from '@angular/router';
     styleUrls: ['./mainPage.component.scss']
 })
 export class MainPageComponent {
+    tasks: Array<Task> =[
+        { name: '1', description: '1', complexity: 4, solved: true} as Task,        
+        { name: '2', description: '2', complexity: 2, solved: true} as Task,
+        { name: '3', description: '3', complexity: 3, solved: false} as Task
+    ];
 
+    solved: boolean = false; 
     constructor(private router: Router) {
 
     }
