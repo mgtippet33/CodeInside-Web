a6050f487f77f7e6a13480991dcf9c1dbcf03c39
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-11-10 23:17:29 +0200

    fixed CORS Privacy problem

--- server.js
+++ server.js
@@ -1,8 +1,33 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+
 const app = express();
+
+const whitelist = ['https://codeinside.herokuapp.com/'];
+
+const corsOptions = {
+    origin: function (origin, callback) {
+        if (!origin) {
+            return callback(null, true);
+        }
+
+        if (whitelist.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+app.use(cors(corsOptions));
+
 app.use(express.static(__dirname + '/dist/codeInside'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+
-'/dist/codeInside/index.html'));});
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname +
+        '/dist/codeInside/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
