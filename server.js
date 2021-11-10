8b6c20470244b56e99cfbdf3b6f114e81d25dde8
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-11-11 00:01:12 +0200

    fixed CORS problem#2

--- server.js
+++ server.js
@@ -4,24 +4,7 @@ const cors = require('cors');
 
 const app = express();
 
-const whitelist = ['https://codeinside.herokuapp.com'];
-
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (!origin) {
-            return callback(null, true);
-        }
-
-        if (whitelist.indexOf(origin) === -1) {
-            var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-            return callback(new Error(msg), false);
-        }
-        return callback(null, true);
-    }
-}
-
-app.use(cors(corsOptions));
+app.options('*', cors())
 
 app.use(express.static(__dirname + '/dist/codeInside'));
 
