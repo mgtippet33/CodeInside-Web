931f5f0469bf3b00c39c8ba547265af13b71c26e
Matvii <72960397+mgtippet33@users.noreply.github.com>
2021-11-23 13:47:11 +0200

    fixed project build #2

--- server.js
+++ server.js
@@ -13,11 +13,11 @@ var corsOptions = {
     res.json({msg: 'This is CORS-enabled for only example.com.'})
   })
 
-app.use(express.static(__dirname + '/dist/codeInside'));
+app.use(express.static(__dirname + '/dist/CodeInside-Web'));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/codeInside/index.html'));
+        '/dist/CodeInside-Web/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
